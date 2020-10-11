4d8740ef294b75fdd25c806d301eb7c29f8aefd3
930lol <vikashsingh7209482955@gmail.com>
2020-10-11 20:52:17 +0530

    change the digit containing all 0 to 1

---
+++ 0to1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int n,r,s=0,b=10,o=1,d=0,w=10;
+	printf("enter the number want to conver 0 into 1:");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		r=n%10;
+		if(r==0)
+		{
+			s=s*b+o;
+		//	b=b*10;
+		}
+		else
+		{
+			s=s*b+r;
+		//	b=b*10;
+		}
+		n=n/10;
+	}
+	printf("%d\n",s);
+	while(s!=0)
+	{
+		r=s%10;
+		d=d*w+r;
+		s=s/10;
+	}
+	printf("%d is number",d);
+	return 0;
+}
