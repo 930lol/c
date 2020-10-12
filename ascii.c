bc78581c04060aa3d3fb46584755a7ef64a92b58
930lol <vikashsingh7209482955@gmail.com>
2020-10-12 09:38:50 +0530

    ascii value

---
+++ ascii.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>  
+int main()
+{
+  char a;
+  
+  printf("\n Kindly insert any character \n");
+  scanf("%c",&a);
+  
+  printf("\n The ASCII value of inserted character = %d",a);
+  return 0;
+}
