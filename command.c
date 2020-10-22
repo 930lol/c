7ac9b9d66ea3f1d36c0f08fc8f2ca7bcd4a778c8
930lol <vikashsingh7209482955@gmail.com>
2020-10-22 23:55:58 +0530

    commanfd line

---
+++ command.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+	int i;
+	printf("Please use program value1 value2 .....\n ");
+	return -1;
+	for(i=1;i<argv;i++)
+	{
+		prtinf("arg[%d] %d n",(argv[i]));
+	}
+	return 0;
+}
