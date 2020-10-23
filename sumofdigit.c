0d69f2f590c630eaf35e3aad5d175612e85608b1
930lol <vikashsingh7209482955@gmail.com>
2020-10-23 23:10:14 +0530

    remove number from the string and add the tatal number in the intreger

---
+++ sumofdigit.c
@@ -0,0 +1,25 @@
+#include  <stdio.h>
+int main()
+{
+    //Initializing variables.
+    char str[100];
+    int i,sum = 0;
+    
+    //Accepting inputs.
+    printf("Enter the string:\n");
+    gets(str);
+    
+    //Iterating each character through for loop.
+    for (i= 0; str[i] != '\0'; i++)
+    {
+        if ((str[i] >= '0') && (str[i] <= '9'))  //Checking for numeric characters.
+        {
+            
+            sum += (str[i] - '0'); //Adding numeric characters.
+
+        }
+    }
+    //Printing result.
+    printf("Sum of all digits:\n%d", sum);
+    return 0; 
+}
