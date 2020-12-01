0bfb67f019b3e63dea8f4f864858e3026caf5e90
930lol <vikashsingh7209482955@gmail.com>
2020-12-01 18:05:00 +0530

    find the median of the two sorted array using pointer

---
+++ pionterfind.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	int a[n],b[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&b[i]);
+	}
+	findmedian(a,b,n);
+	return 0;
+}
+int findmedian(int* a,int* b,int n)
+{
+	int i,c[n+n],k=0,j,t,r;
+	for(i=0;i<n;i++)
+	{
+		c[k]=*a;
+		k++;
+		a++;
+	}
+	for(i=0;i<n;i++)
+	{
+		c[k]=*b;
+		k++;
+		b++;
+	}
+	for(i=0;i<k;i++)
+	{
+		for(j=i+1;j<k;j++)
+		{
+			if(c[i]>c[j])
+			{
+				t=c[i];
+				c[i]=c[j];
+				c[j]=t;
+			}
+		}
+	}
+	for(i=0;i<k;i++)
+	{
+		printf("%d ",c[i]);
+	}
+	if(k%2==0)
+	{
+		r=(c[k/2]+c[k/2-1])/2;
+	}
+	else
+	{
+		r=c[k/2-1];
+	}
+	printf("The median of the sorted array \n%d",r);
+}
